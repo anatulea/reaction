8f34562069f423c6c76bf76d145e3c4caf858df2
Ana Tulea <anatulea@Anas-MacBook-Pro.local>
2020-10-28 18:42:37 -0700

    Publish Message Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import PublishMessage from "./PublishMessage";
 class App extends Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+    <div>
+    <h2>Reaction</h2>  
+    <hr/>
+    <PublishMessage/>
+    </div>);
   }
 }
 export default App;
